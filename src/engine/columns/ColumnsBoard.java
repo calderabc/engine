b38a209b0391dbbdc5c02d9bab4cf85e29d71895
hexregex <acalder@deep-thought.localdomain>
2017-10-01 09:41:54 -0600

    Fixed a bug in the Columns block removal algorithm.

--- src/engine/columns/ColumnsBoard.java
+++ src/engine/columns/ColumnsBoard.java
@@ -27,98 +27,61 @@ public class ColumnsBoard extends SimpleBoard {
 	}
 
 
-	private void terminateBlocks(Block[] blocksJustLanded,
-	                             Set<Block> deathRow,
-	                             Block... blocks) {
-		for (Block terminalBlock : blocks) {
-			deathRow.add(terminalBlock);
-			// If this block happens to be part of the same piece there is no reason
-			// to check again so take it out of the running.
-			for (int i = 0; i < blocksJustLanded.length; i++) {
-				if (blocksJustLanded[i] == terminalBlock) {
-					blocksJustLanded[i] = null;
-				}
-			}
-		}
-
-	}
-
-	// TODO: Fix bug. Noticed the following situation
-		/*
-				#
-				*  falling piece
-				*
-
-				 *
-				*#* landed blocks
-				*##
-
-		Does not remove bottom "*".
-
-		result is:
-
-				##
-			    *##
-
-		 */
-
-	// Remove blocks with three or more of the same type in a row from board.
+	// Remove blocks which form straight lines of 3 or more of the same
+	// block type in a row vertically, diagnally, or horizontally.
 	@Override
 	public int tryRemoveBlocks(Block[] blocksJustLanded) {
 		if (blocksJustLanded.length == 0) return 0; // Nothing (more) to do.
 
-		// Use a set so terminal blocks won't be added multiple times.
+		// Used a Set so terminal blocks won't be added multiple times.
 		Set<Block> deathRow = new HashSet<>();
 
 		for (Block currBlock : blocksJustLanded) {
 			if (currBlock == null) continue;
 
-			Block[] candidates = new Block[8];
+			// TODO: Surely this algorithm can be made more efficient.
+			// Combine 2 or all of the for loops.
+
+			Block[] suspects = new Block[8];
 			// Get adjacent blocks which are of the same type.
-			// Add to candidate array in circular order around the block.
+			// Add to suspects array in circular order around the block.
 			for (int i = 0; i < 8; i++) {
 				Block testBlock = null;
 				try {
 					testBlock = getBlock(
-					Coordinates.add(currBlock.pos, aroundCoords[i])
+						Coordinates.add(currBlock.pos, aroundCoords[i])
 					);
 				} catch (ArrayIndexOutOfBoundsException e) {};
 				if (testBlock != null && currBlock.type == testBlock.type) {
-					candidates[i] = testBlock;
+					suspects[i] = testBlock;
 				}
 			}
 			// If this block is directly between two matching blocks
 			// remove all three blocks.
 			for (int i = 0; i < 4; i++) {
-				if (candidates[i] != null && candidates[i + 4] != null) {
-					terminateBlocks(blocksJustLanded,
-					                deathRow,
-					                candidates[i],
-					                candidates[i + 4]);
+				if (suspects[i] != null && suspects[i + 4] != null) {
+					deathRow.add(suspects[i + 4]);
 					deathRow.add(currBlock);
+					deathRow.add(suspects[i]);
 				}
 			}
 			// If any of the adjacent matching blocks are directly between this
 			// block and another matching block remove all three blocks.
 			for (int i = 0; i < 8; i++) {
-				if (candidates[i] != null) {
+				if (suspects[i] != null) {
 					Block testBlock = null;
 					try {
 						testBlock = getBlock(
-						Coordinates.add(candidates[i].pos, aroundCoords[i])
+						Coordinates.add(suspects[i].pos, aroundCoords[i])
 						);
 					} catch (ArrayIndexOutOfBoundsException e) {};
 				    if (testBlock != null && currBlock.type == testBlock.type) {
-				    	terminateBlocks(blocksJustLanded,
-					                    deathRow,
-					                    candidates[i],
-					                    testBlock);
 				    	deathRow.add(currBlock);
+					    deathRow.add(suspects[i]);
+					    deathRow.add(testBlock);
 				    }
 				}
 			}
-			System.out.println("got here");
-
 		}
 
 		Set<Block> theFallen = new HashSet();
@@ -135,7 +98,7 @@ public class ColumnsBoard extends SimpleBoard {
 					aBlockAbove.visual.update(aBlockAbove);
 				}
 			}
-			// Be sure space at top of column is empty.
+			// Be sure tile at top of column is empty.
 			blockMatrix[x][0] = null;
 			// Remove block from screen.
 			terminalBlock.terminate();
